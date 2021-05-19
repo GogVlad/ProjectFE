e35be2f87ccf6139672dc363504bfbfb195f7ff0
RaduLepadatu <radu.lepadatu@principal33.com>
2021-05-19 13:10:36 +0300

    added Login page.
    deleted HelloWorld page

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Login />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from "./login/Login.vue";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Login,
+  },
+};
 </script>
 
 <style>
